0e680198cc8a3adf517ecb29d92af43c5bb74121
Jonas-Quinn <syntractrix@gmail.com>
2020-05-21 16:15:30 +0200

    Upgraded site view

--- mysite/settings.py
+++ mysite/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = (os.environ.get('DUBUG_VALUE') == 'True')  #need to change U
 # DEBUG = True
 
-ALLOWED_HOSTS = ['bluebazaar.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['bluebazaar.herokuapp.com', 'localhost', '127.0.0.1', '192.168.8.101']
 #ALLOWED_HOSTS = []
 
 
